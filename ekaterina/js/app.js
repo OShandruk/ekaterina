db6be025742311a1cf880da513c9db082e7e5616
Oleksandr Shandruk <sasha_shandruk1991@ukr.net>
2021-02-01 22:52:25 +0200

    add script in index

--- ekaterina/js/app.js
+++ ekaterina/js/app.js
@@ -1573,29 +1573,6 @@ function scroll_animate(event) {
 // });
 
 
-
-
-
-
-// **************************leader-line***********************************
-const startElement = document.getElementById('location__dotted-start1');
-const	 endElement = document.getElementById('location__dotted-end1');
-var line1 = new LeaderLine(startElement, endElement, {
-	color: '#B98B4B', 
-	size: 2,
-	dash:true,
-	endPlug:'behind',
-});
-const startElement2 = document.getElementById('location__dotted-start2');
-const endElement2 = document.getElementById('location__dotted-end2');
-var line2 = new LeaderLine(startElement2, endElement2, {
-	color: '#B98B4B', 
-	size: 2,
-	dash:true,
-	endPlug:'behind',
-});
-
-
 //******************************* */ TOOLTIP***************************
 
 const location1 = document.getElementById('location__tooltip1');
